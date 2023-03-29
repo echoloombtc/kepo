51dd53a7d06daddae7a2d92816b62b79d9ea2fce
llowq <96781765+kenDevXD@users.noreply.github.com>
2023-03-29 17:38:17 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# kepo
-yayaa
+<!DOCTYPE html>
+<h2 align="center">
+ XRAY Multiport Websocket Free By kenDevXD 
+<br> Support custom path / worryfree
+  
+<h2 align="center"> ♦️Supported Linux Distribution♦️</h2>
+<p align="center"><img src="https://d33wubrfki0l68.cloudfront.net/5911c43be3b1da526ed609e9c55783d9d0f6b066/9858b/assets/img/debian-ubuntu-hover.png"width="400"></p>
+<p align="center"><img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%2010&message=Buster&color=red"> <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=ubuntu&label=Ubuntu%2018.04&message=LTS&color=orange"></p>
+
+♦️ For Debian 10 Only For First Time Installation (Update Repo) <br>
+ 
+  ```html
+ apt update -y && apt upgrade -y && apt dist-upgrade -y && reboot
+  ```
+  ♦️ For Ubuntu 18.04 Only For First Time Installation (Update Repo) <br>
+  
+  ```html
+ apt-get update && apt-get upgrade -y && apt dist-upgrade -y && update-grub && reboot
+ ```
+♦️ Installation Link<br>
+
+  ```html
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl && wget https://raw.githubusercontent.com/kenDevXD/kepo/main/setup.sh && chmod +x setup.sh && ./setup.sh
+  ```
+<b>
+
+## ⏩ XRAY MULTIPORT WEBSOCKET AUTOSCRIPT DETAILS ⏪
+<b>
+[ XRAY SERVICES ] <br>
+<br>
+♦️ XRAY VMESS WEBSOCKET TLS & NON-TLS 443/80<br>
+♦️ XRAY VLESS WEBSOCKET TLS & NON-TLS 443/80 (SUPPORT CUSTOMPATH /WORRYFREE)<br>
+♦️ XRAY TROJAN WEBSOCKET TLS & NON-TLS 443/80<br>
+♦️ XRAY TROJAN TCP XTLS 443<br>
+♦️ XRAY TROJAN TCP TLS 443<br>
+<br>
+[ OTHER SERVICES ] <br>
+<br>
+♦️ SUPPORT CUSTOM PATH BY DHARAK36 XRAY-CORE <br>
+♦️ NEW UPDATE BBRPLUS 5.15.96 <br>
+♦️ BANDWITH MONITOR <br>
+♦️ RAM MONITOR <br>
+♦️ YAML LINK <br>
+♦️ DNS CHANGER <br>
+♦️ NETFLIX REGION CHECKER <br>
+♦️ CHECK LOGIN USER <br>
+♦️ CHECK CREATED CONFIG <br>
+♦️ AUTOMATIC CLEAR LOG <br>
+♦️ AUTOMATIC VPS REBOOT <br>
+♦️ AUTOMATIC DELETE EXPIRED CONFIG <br>
+♦️ BACKUP & RESTORE <br></br>
---
+++ menu.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+clear
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
+## Foreground
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+#Domain & IPVPS
+domain=$(cat /root/domain)
+IPVPS=$(curl -s ipinfo.io/ip)
+# OS Uptime
+uptime="$(uptime -p | cut -d " " -f 2-10)"
+# RAM Info
+tram=$(free -m | awk 'NR==2 {print $2}')
+uram=$(free -m | awk 'NR==2 {print $3}')
+# Total BANDWIDTH
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $9" "substr ($10, 1, 1)}')"
+echo ""
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "      ${WB}Multiport Websocket Autoscript By AIXXY7${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${WB}»»» Server Information «««${NC}             "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}OS      :  "$(hostnamectl | grep "Operating System" | cut -d ' ' -f5-) ${NC}         
+echo -e "  ${RB}♦️${NC} ${YB}KERNEL  :  $(uname -r) ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}UPTIME  :  $uptime ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}RAM     :  $uram MB / $tram MB ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}DOMAIN  :  $domain ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}IPVPS   :  $IPVPS ${NC} "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                    ${WB}»»» XRAYS Menu «««${NC}          "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}01.${NC} ${YB}XRAY VMESS WS${NC}"
+echo -e "  ${RB}02.${NC} ${YB}XRAY VLESS WS${NC}"
+echo -e "  ${RB}03.${NC} ${YB}XRAY TROJAN WS${NC}"
+echo -e "  ${RB}04.${NC} ${YB}XRAY TROJAN XTLS${NC}"
+echo -e "  ${RB}05.${NC} ${YB}XRAY TROJAN TCP${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                     ${WB}»»» VPS Menu «««${NC}                 "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}06.${NC} ${YB}INSTALL ADS BLOCK ${NC}"
+echo -e "  ${RB}07.${NC} ${YB}INSTALL TCP BBRPLUS ${NC}"
+echo -e "  ${RB}08.${NC} ${YB}ADS BLOCK MENU ${NC}"
+echo -e "  ${RB}09.${NC} ${YB}DNS CHANGER ${NC}"
+echo -e "  ${RB}10.${NC} ${YB}NETFLIX CHECKER ${NC}"
+echo -e "  ${RB}11.${NC} ${YB}LIMIT BANDWITH SPEED ${NC}"
+echo -e ""
+echo -e "  ${RB}12.${NC} ${YB}CHANGE DOMAIN ${NC}"
+echo -e "  ${RB}13.${NC} ${YB}RENEW CERT XRAYS ${NC}"
+echo -e "  ${RB}14.${NC} ${YB}CHECK VPN STATUS ${NC}"
+echo -e "  ${RB}15.${NC} ${YB}CHECK VPN PORT ${NC}"
+echo -e "  ${RB}16.${NC} ${YB}RESTART VPN SERVICE ${NC}"
+echo -e "  ${RB}17.${NC} ${YB}SPEEDTEST ${NC}"
+echo -e "  ${RB}18.${NC} ${YB}CHECK CPU & RAM ${NC}"
+echo -e "  ${RB}19.${NC} ${YB}CHECK BANDWIDTH ${NC}"
+echo -e "  ${RB}20.${NC} ${YB}BACKUP ${NC}"
+echo -e "  ${RB}21.${NC} ${YB}RESTORE ${NC}"
+echo -e "  ${RB}22.${NC} ${YB}REBOOT ${NC}"
+echo -e "  ${RB}23.${NC} ${YB}EXIT ${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                  ${WB}»»» Total Bandwith «««${NC}             "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Daily Data Usage    : $ttoday ${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Monthly Data Usage  : $tmon ${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo ""
+read -p "Select From Options [ 1 - 22 ] : " menu
+case $menu in
+1)
+clear
+menu-ws
+;;
+2)
+clear
+menu-vless
+;;
+3)
+clear
+menu-tr
+;;
+4)
+clear
+menu-xrt
+;;
+5)
+clear
+menu-xtr
+;;
+6)
+clear
+ins-helium
+;;
+8)
+clear
+helium
+;;
+9)
+clear
+dns
+;;
+10)
+clear
+nf
+;;
+7)
+clear
+bbr
+;;
+11)
+clear
+limit
+;;
+12)
+clear
+add-host
+;;
+13)
+clear
+certxray
+;;
+14)
+clear
+status
+;;
+15)
+clear
+info
+;;
+16)
+clear
+restart
+;;
+17)
+clear
+speedtest
+;;
+18)
+clear
+htop
+;;
+23)
+clear
+exit
+;;
+20)
+clear
+backup
+;;
+21)
+clear
+restore
+;;
+22)
+clear
+reboot
+;;
+19)
+clear
+vnstat
+;;
+*)
+clear
+menu
+;;
+esac
